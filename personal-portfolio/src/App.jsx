01a972e0d48f710461df595f47bd26a17fbba443
Rick Moore <rick329mo@gmail.com>
2023-01-17 16:32:29 -0600

    got routes set up, started styling

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,12 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from "./pages/Home"
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import AboutMe from './pages/AboutMe'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import "./styles.css"
 
 function App() {
  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <div className='flex'>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/aboutme">About Me</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/aboutme' element={<AboutMe />} /> 
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
