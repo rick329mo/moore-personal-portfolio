55442d34f9ce9d0adf3539bc253683f08c953431
Rick Moore <rick329mo@gmail.com>
2023-03-13 21:35:14 -0500

    changed title

--- personal-portfolio/src/pages/Home.jsx
+++ personal-portfolio/src/pages/Home.jsx
@@ -1,11 +1,10 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Hi, <br/> I'm <span className="accent">Rick Moore</span>, 
-            <br/> web developer.</h1>
+            <div>
+                <h1>Hello, <br/> I'm <span className="accent">Rick Moore</span>, 
+                <br/> web developer.</h1>
 
-            <p className="secondary">front-end developer / UI designer</p>
-        </div>
-
+                <p className="secondary">front-end developer / UI designer</p>
+            </div>
     )
 }
